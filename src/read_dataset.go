28c73073bff3f26940b78ff625e7f712858cff50
kartik-dutt <kartik.dutt@zomato.com>
2021-01-24 12:27:11 +0530

    Read dataset package

--- src/read_dataset.go
+++ src/read_dataset.go
@@ -1,6 +1,7 @@
 package read_dataset
 
 import (
+	"encoding/xml"
 	"os"
 
 	error_handler "github.com/kartik-dutt/Simple-Search-Engine/error_handler"
@@ -12,11 +13,29 @@ type document struct {
 	Title string `xml:"title"`
 	URL   string `xml:"url"`
 	Text  string `xml:"abstract"`
-	ID    int32
+	ID    int
 }
 
 func ReadDataset(filepath string) ([]document, error) {
 	xmlFile, err := os.Open(filepath)
 	error_handler.ErrorHandler("Error opening "+filepath, err)
+	defer xmlFile.Close()
 
+	dec := xml.NewDecoder(xmlFile)
+	dump := struct {
+		Documents []document `xml: "doc"`
+	}{}
+
+	if err := dec.Decode(&dump); err != nil {
+		error_handler.ErrorHandler("Error decoding xml file.", err)
+		return nil, err
+	}
+
+	docs := dump.Documents
+	// ID is simple the index.
+	for i := range docs {
+		docs[i].ID = i
+	}
+
+	return docs, nil
 }
