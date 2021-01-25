e6bdef73d7e3d150212c1c8b4f84fac449df4d88
kartik-dutt <kartik.dutt@zomato.com>
2021-01-25 22:23:11 +0530

    Complete search engine GUI :fire:

--- src/query.go
+++ src/query.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"os"
 
-	inverted_index "github.com/kartik-dutt/Simple-Search-Engine/src/inverted_index"
+	"fyne.io/fyne"
+	app "fyne.io/fyne/app"
+	"fyne.io/fyne/container"
+	"fyne.io/fyne/widget"
+	"github.com/kartik-dutt/Simple-Search-Engine/src/inverted_index"
 	read_dataset "github.com/kartik-dutt/Simple-Search-Engine/src/read_dataset"
 	text_cleaner "github.com/kartik-dutt/Simple-Search-Engine/src/text_cleaner"
 )
@@ -57,28 +61,82 @@ func search(index map[string][]int, text string) []int {
 	return res
 }
 
+func DocToString(doc read_dataset.Document) string {
+	s := doc.Title + "\n" + doc.URL + "\n" + doc.Text + "\n"
+	return s
+}
+
 func main() {
-	jsonFile, err := os.Open("./../config/dataset.json")
-	if err != nil {
-		log.Fatal("Error in Opening File!")
-		panic(err)
-	}
+	appl := app.New()
+	win := appl.NewWindow("Your-Personal-Search-Engine")
+	disp := "Loading Dataset Configurations!\n"
+	win.SetContent(widget.NewLabel(disp))
+	win.Resize(fyne.NewSize(800, 800))
+	go func() {
+		jsonFile, err := os.Open("./../config/dataset.json")
+		if err != nil {
+			log.Fatal("Error in Opening File!")
+			panic(err)
+		}
 
-	fmt.Println("Succesfully opened config/datset.json")
-	defer jsonFile.Close()
+		disp += "Succesfully opened config/datset.json\n"
+		disp += "=====================================\n\n"
+		log.Println("Succesfully opened config/datset.json")
+		defer jsonFile.Close()
+		win.SetContent(widget.NewLabel(disp))
 
-	byteArr, _ := ioutil.ReadAll(jsonFile)
-	var datasets Datasets
-	json.Unmarshal(byteArr, &datasets)
-	docs := make([]read_dataset.Document, 0)
-	for _, dataset := range datasets.DatasetsList {
-		fmt.Println("Opening ./../data/" + dataset.Name)
-		doc, _ := read_dataset.ReadDataset("../data/" + dataset.Name)
-		docs = append(docs, doc...)
-		fmt.Println("Closed!")
-	}
+		byteArr, _ := ioutil.ReadAll(jsonFile)
+		var datasets Datasets
+		json.Unmarshal(byteArr, &datasets)
+		docs := make([]read_dataset.Document, 0)
+		disp += "Loading Dataset! \nThis may take 1 - 2 minutes as nearly 1 million urls are being loaded.\n"
+		win.SetContent(widget.NewLabel(disp))
+		for _, dataset := range datasets.DatasetsList {
+			log.Println("Opening ./../data/" + dataset.Name)
+			disp = disp + "Opening ./../data/" + dataset.Name
+			win.SetContent(widget.NewLabel(disp))
+			doc, _ := read_dataset.ReadDataset("../data/" + dataset.Name)
+			docs = append(docs, doc...)
+			disp += "\nClosed!\n"
+			win.SetContent(widget.NewLabel(disp))
+			log.Println("Closed!")
+		}
 
-	index := make(map[string][]int)
-	index = inverted_index.InvertedIndex(index, docs)
-	fmt.Println(search(index, "small wild cat"))
+		disp += "Loaded Dataset!\n"
+		disp += "=====================================\n\n"
+		win.SetContent(widget.NewLabel(disp))
+
+		index := make(map[string][]int)
+		disp += "Indexing the dataset!"
+		win.SetContent(widget.NewLabel(disp))
+		index = inverted_index.InvertedIndex(index, docs)
+		disp += "\nIndexed Dataset!\n"
+		disp += "================================\n\n"
+		win.SetContent(widget.NewLabel(disp))
+
+		input := widget.NewEntry()
+		input.SetPlaceHolder("Search : ")
+		searchRes := widget.NewLabel("Search Results")
+		content := container.NewVBox(input, widget.NewButton("Search", func() {
+			log.Println("Content was:", input.Text)
+			txt := ""
+			if input.Text != "" {
+				res := search(index, input.Text)
+				if len(res) > 0 {
+					res = res[:int(math.Max(float64(len(res)), 10))]
+				}
+				for _, idx := range res {
+					txt += ("\n" + DocToString(docs[idx]))
+				}
+
+				searchRes.SetText(txt)
+				log.Println(txt)
+			}
+		}), searchRes)
+
+		win.SetContent(widget.NewScrollContainer(content))
+	}()
+
+	win.ShowAndRun()
+	return
 }
